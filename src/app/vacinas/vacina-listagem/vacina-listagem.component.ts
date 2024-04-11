00be6203c790fedefdf9d7d9a791e41b59be334e
Icaleb1 <calebfernandes080@gamil.com>
2024-04-11 11:05:39 -0300

    Criação dos modulos de vacina

---
+++ src/app/vacinas/vacina-listagem/vacina-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vacina-listagem',
+  standalone: true,
+  imports: [],
+  templateUrl: './vacina-listagem.component.html',
+  styleUrl: './vacina-listagem.component.scss'
+})
+export class VacinaListagemComponent {
+
+}
