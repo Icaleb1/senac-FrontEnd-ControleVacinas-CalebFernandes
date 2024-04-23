d176f5ed1559fb58c4049ce9d19644bd215af2d7
Icaleb1 <calebfernandes080@gamil.com>
2024-04-23 20:50:26 -0300

    Inicio da tela de cadastro e progresso na aba de navegação;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'vacinas', pathMatch: 'full'},
-  {
+   {
     path: 'vacinas',
     loadChildren: ()=>
       import('./vacinas/vacinas.module').then((m) => m.VacinasModule),
---
+++ src/app/vacinas/vacina-detalhe/vacina-detalhe.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vacina-detalhe',
+  templateUrl: './vacina-detalhe.component.html',
+  styleUrls: ['./vacina-detalhe.component.scss']
+})
+export class VacinaDetalheComponent implements OnInit{
+vacina: any;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
